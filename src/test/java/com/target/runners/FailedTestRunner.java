3dbacc6442a67d97395598ace4c070cc9532cec6
Nora <nurgul3374@gmail.com>
2025-12-03 21:15:41 -0500

    Added: CukesRunner FailedTestRunner Hooks

---
+++ src/test/java/com/target/runners/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package com.target.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue="com/target/library/step_definitions"
+)
+
+public class FailedTestRunner {
+}
